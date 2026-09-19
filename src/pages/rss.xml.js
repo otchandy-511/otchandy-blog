514193cf4095a1b3d48f15fddb5bc2e6fbbe784f
otchandy-511 <ot.chandy@gmail.com>
2026-09-19 11:51:27 +0700

    Create rss.xml.js

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE } from '../lib/site';
+
+export async function GET(context) {
+  const posts = await getCollection('posts', ({ data }) => !data.draft);
+  return rss({
+    title: SITE.title,
+    description: SITE.description,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.publishDate,
+      link: `/posts/${post.slug}/`,
+    })),
+  });
+}
